3eced130b9aec0c06c03daaedd2177183a0e9d75
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-16 13:40:39 +0200

    add color system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#050505',
+        'primary-12': '#1F1F1F',
+        'primary-18': '#2D2D2D',
+        'primary-23': '#3A3A3A',
+        'secondary': '#757575',
+        'secondary-91': '#E9E9E9',
+        'secondary-96': '#F4F4F4',
+        'accent': '#A445ED',
+        'error': 'FF5252'
+      }
+    },
   },
   plugins: [],
 }
