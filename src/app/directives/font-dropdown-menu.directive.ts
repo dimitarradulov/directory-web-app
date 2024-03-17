8fd8447794ef84ed6ed6a5b6cd6968d96c049cdb
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-17 15:39:31 +0200

    inject template and view container refs

--- src/app/directives/font-dropdown-menu.directive.ts
+++ src/app/directives/font-dropdown-menu.directive.ts
@@ -1,7 +1,15 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  TemplateRef,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 
 @Directive({
   selector: '[appFontDropdownMenu]',
   standalone: true,
 })
-export class FontDropdownMenuDirective { }
+export class FontDropdownMenuDirective {
+  private templateRef: TemplateRef<any> = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
+}
