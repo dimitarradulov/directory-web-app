d4dd0b3b9f10bc3c47b5de9b09eb1a431dbacb4c
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-17 15:46:45 +0200

    add font dropdown component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { FontDropdownComponent } from '../font-dropdown/font-dropdown.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontDropdownComponent],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
