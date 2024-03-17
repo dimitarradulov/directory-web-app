2004abfd4a43186de66838a15d7598e4dc34741f
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-17 15:25:51 +0200

    add handling for clicking outside the dropdown

--- src/app/directives/font-dropdown-manager.directive.ts
+++ src/app/directives/font-dropdown-manager.directive.ts
@@ -1,10 +1,29 @@
-import { Directive, WritableSignal, computed, signal } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  WritableSignal,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 
 @Directive({
   selector: '[appFontDropdownManager]',
   standalone: true,
 })
 export class FontDropdownManagerDirective {
+  private elementRef: ElementRef<HTMLElement> = inject(ElementRef);
+
   private isShowingSignal: WritableSignal<boolean> = signal(false);
   isShowing = computed(() => this.isShowingSignal());
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: PointerEvent) {
+    const nativeElement: any = this.elementRef.nativeElement;
+    const clickedInside: boolean = nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.isShowingSignal.set(false);
+    }
+  }
 }
