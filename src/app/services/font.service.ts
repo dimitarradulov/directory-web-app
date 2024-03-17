146302d887b2fa86d8322984831a81ff9ff7b6c1
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-17 15:15:40 +0200

    add font signal + api to update it & expose it

--- src/app/services/font.service.ts
+++ src/app/services/font.service.ts
@@ -1,6 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal, computed, signal } from '@angular/core';
+
+import { Font } from '../models/font.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FontService {}
+export class FontService {
+  private currentFont: WritableSignal<Font> = signal(Font.SANS);
+  font = computed(() => this.currentFont());
+
+  setFont(font: Font): void {
+    this.currentFont.set(font);
+  }
+}
