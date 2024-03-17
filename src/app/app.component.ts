5d68ef48826cda719320603209828b6a1dcfe4d3
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-17 16:46:45 +0200

    inject font service + set font from it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
+import { FontService } from './services/font.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  public fontService = inject(FontService);
+}
