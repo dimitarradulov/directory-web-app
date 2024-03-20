d3df409b79f3ae5461832d5aaad782abe555c8c2
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-20 18:25:44 +0200

    import ThemeSwitcherDirective

--- src/app/theme-switcher/theme-switcher.component.ts
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { ThemeSwitcherDirective } from '../directives/theme-switcher.directive';
 
 @Component({
   selector: 'app-theme-switcher',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, ThemeSwitcherDirective],
   templateUrl: './theme-switcher.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ThemeSwitcherComponent { }
+export class ThemeSwitcherComponent {}
