c5b66a89edc5920bddfb64550ae00a46a33524fe
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-20 17:59:44 +0200

    add necessary methods for theme switching

--- src/app/services/theme-switcher.service.ts
+++ src/app/services/theme-switcher.service.ts
@@ -5,6 +5,44 @@ import { Theme } from '../models/theme.model';
   providedIn: 'root',
 })
 export class ThemeSwitcherService {
+  private readonly localStorageKey = 'theme';
   private currentTheme: WritableSignal<Theme> = signal(Theme.LIGHT);
   theme = computed(() => this.currentTheme());
+
+  constructor() {
+    this.setTheme(this.getTheme(), false);
+  }
+
+  setTheme(theme: Theme, saveToLocalStorage = true): void {
+    this.addThemeClassToDocumentElement(theme);
+    this.currentTheme.set(theme);
+
+    if (saveToLocalStorage) {
+      localStorage.setItem(this.localStorageKey, theme);
+    }
+  }
+
+  private getTheme(): Theme {
+    if (this.isDarkModePreferred()) {
+      return Theme.DARK;
+    }
+
+    return Theme.LIGHT;
+  }
+
+  private addThemeClassToDocumentElement(theme: Theme): void {
+    if (theme === Theme.DARK) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  private isDarkModePreferred(): boolean {
+    return (
+      localStorage[this.localStorageKey] === 'dark' ||
+      (!(this.localStorageKey in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    );
+  }
 }
