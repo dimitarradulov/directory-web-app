f1efedcd94782d4782a89b179cd5fa0b3ec60234
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-22 10:22:56 +0200

    Update font-dropdown.component.ts

--- src/app/font-dropdown/font-dropdown.component.ts
+++ src/app/font-dropdown/font-dropdown.component.ts
@@ -9,7 +9,6 @@ import {
 import { FontService } from '../services/font.service';
 import { Font } from '../models/font.model';
 import { FontDropdownManagerDirective } from '../directives/font-dropdown-manager.directive';
-import { FontDropdownMenuDirective } from '../directives/font-dropdown-menu.directive';
 import { FontDropdownTriggerDirective } from '../directives/font-dropdown-trigger.directive';
 
 @Component({
@@ -18,7 +17,6 @@ import { FontDropdownTriggerDirective } from '../directives/font-dropdown-trigge
   imports: [
     CommonModule,
     FontDropdownManagerDirective,
-    FontDropdownMenuDirective,
     FontDropdownTriggerDirective,
   ],
   templateUrl: './font-dropdown.component.html',
