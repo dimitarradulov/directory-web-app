ff7b031835fa6df8cc99027d339ea61ff248635a
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-23 16:18:11 +0200

    add DictionaryWordInfoComponent + gap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FontService } from './services/font.service';
 import { DictionarySearchComponent } from './dictionary-search/dictionary-search.component';
+import { DictionaryWordInfoComponent } from './dictionary-word-info/dictionary-word-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, DictionarySearchComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    DictionarySearchComponent,
+    DictionaryWordInfoComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
