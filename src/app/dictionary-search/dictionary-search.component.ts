9278c93a188bd08e4fa02ea6f966e4aa3e2713e3
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-23 10:16:44 +0200

    add form

--- src/app/dictionary-search/dictionary-search.component.ts
+++ src/app/dictionary-search/dictionary-search.component.ts
@@ -1,13 +1,12 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dictionary-search',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dictionary-search.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DictionarySearchComponent { }
+export class DictionarySearchComponent {}
