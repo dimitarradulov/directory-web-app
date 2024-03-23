bc3dfa76750d0b8890494f3bfdca961fdcee5e0e
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-23 10:09:49 +0200

    import dictionary search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { FontService } from './services/font.service';
+import { DictionarySearchComponent } from './dictionary-search/dictionary-search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, DictionarySearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
