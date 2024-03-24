4b628d3369d99f51fd514565dea9f3371de6cc80
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-24 13:34:50 +0200

    add signal input for font & use to apply 'italic'

--- src/app/dictionary-word-info/dictionary-word-info.component.ts
+++ src/app/dictionary-word-info/dictionary-word-info.component.ts
@@ -1,13 +1,17 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+import { Font } from '../models/font.model';
 
 @Component({
   selector: 'app-dictionary-word-info',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './dictionary-word-info.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DictionaryWordInfoComponent { }
+export class DictionaryWordInfoComponent {
+  font = input.required<Font>();
+
+  protected readonly Font = Font;
+}
