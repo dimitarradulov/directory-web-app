74ac37dbb29ac538fc78d10fbbece35ff9b76200
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-24 14:23:55 +0200

    compute is italic

--- src/app/dictionary-word-info/dictionary-word-info.component.ts
+++ src/app/dictionary-word-info/dictionary-word-info.component.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 
 import { Font } from '../models/font.model';
+import { WordTypeComponent } from '../word-type/word-type.component';
 
 @Component({
   selector: 'app-dictionary-word-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WordTypeComponent],
   templateUrl: './dictionary-word-info.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DictionaryWordInfoComponent {
   font = input.required<Font>();
+  isItalic = computed(() => this.font() === Font.SANS);
 
   protected readonly Font = Font;
 }
