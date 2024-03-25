3b256f040499a39e9602a0228f572658408e0a76
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-03-25 17:24:29 +0200

    add word api

--- src/app/services/dictionary.service.ts
+++ src/app/services/dictionary.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal, computed, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class DictionaryService {
   private readonly basicUrl =
     'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
+  private wordSignal: WritableSignal<string> = signal<string>('');
+  word = computed(() => this.wordSignal());
+
+  setWord(word: string) {
+    this.wordSignal.set(word);
+  }
 }
